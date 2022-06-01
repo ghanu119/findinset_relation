6e6a473a9166b577258f29133180db9d76728d3e
GhanuZ <ghanshyamashra@gmail.com>
2022-06-02 00:01:36 +0530

    Builder namespacee missing fixed

--- ghanuz/Relations/src/FindInSet/FindInSetOne.php
+++ ghanuz/Relations/src/FindInSet/FindInSetOne.php
@@ -1,6 +1,7 @@
 <?php
 namespace GhanuZ\FindInSet;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Concerns\CanBeOneOfMany;
